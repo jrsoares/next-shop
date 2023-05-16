77d988f3690e6cc4c00836a59ee6bd830cc6b7e6
Jr.Soares <juniorsoaresti@gmail.com>
2023-05-16 10:02:37 -0400

    styling page home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,18 @@ import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import { Container, Header } from "../styles/pages/app";
 import logoImg from "../assets/Logo.svg";
-function MyApp({ Component, pageProps }: AppProps) {
+import Image from "next/image";
+function App({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />;
     </Container>
   );
 }
 
-export default MyApp;
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,29 @@
 import type { NextPage } from "next";
 
-import { styled } from "../styles";
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-
-  span: {
-    fontWeight: "bold",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import image1 from "../assets/camiseta1.png";
+import image2 from "../assets/camiseta2.png";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Button>Teste</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={image1} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={image2} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 };
 
