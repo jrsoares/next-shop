1c2bbb532c9209e74e776fb4712526557aa69760
Jr.Soares <juniorsoaresti@gmail.com>
2023-05-15 22:40:00 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+
+  span: {
+    fontWeight: "bold",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Button>Teste</Button>
+    </div>
+  );
+};
+
+export default Home;
