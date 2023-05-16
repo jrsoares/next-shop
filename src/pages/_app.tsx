201e798544547c2ec03b71b84c3e072caf2f6239
Jr.Soares <juniorsoaresti@gmail.com>
2023-05-16 09:03:44 -0400

    add image logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
-
+import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
+import logoImg from "../assets/Logo.svg";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  globalStyles();
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { styled } from "../styles";
 
 const Button = styled("button", {
-  backgroundColor: "$rocketseat",
+  backgroundColor: "$green300",
   borderRadius: 4,
   border: 0,
   padding: "4px 8px",
